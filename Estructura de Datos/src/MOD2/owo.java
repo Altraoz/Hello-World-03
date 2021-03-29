68936442b9c123afd2b3f6889cf2dfcb8c5fba3c
Altraoz <80478887+Altraoz@users.noreply.github.com>
2021-03-29 18:24:21 -0500

    Holi
    ss

---
+++ Estructura de Datos/src/MOD2/owo.java
@@ -0,0 +1,9 @@
+package MOD2;
+
+import javax.swing.plaf.synth.SynthToolTipUI;
+
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+public class owo {
+    System.out.println("holo");
+}
